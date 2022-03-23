c6fcb9319dc6119aa4d911950d80b32c0f4007ab
A7medFlex <om5280201@gmail.com>
2022-03-23 16:43:55 +0200

    farm

---
+++ src/components/FarmSections/FarmItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="s-question">
+    <div :class="activeQ" @click="toggle($event)">
+      <img class="farm-img" :src="image" alt="" />
+      <div class="farm-data">
+        <h2 class="farm-header">{{ farmHeader }}</h2>
+        <p class="farm-sub-header">{{ farmSubHeader }}</p>
+        <p class="desc">{{ farmDesc }}</p>
+      </div>
+      <div class="statics">
+        <p>
+          Staked <span>{{ staked }}</span>
+        </p>
+        <p>
+          Earned <span>{{ earned }}</span>
+        </p>
+        <p>
+          APR <span>{{ APR }}</span>
+        </p>
+        <p>
+          LP Price <span>{{ LP }}</span>
+        </p>
+        <p>
+          Total Locked <span>{{ total }}</span>
+        </p>
+      </div>
+    </div>
+    <div :class="activeA">
+      <div class="middle">
+        <div class="first">
+          <h3>Deposit</h3>
+          <label for="Balance">Balance:<span>0 TPAD</span></label>
+          <input id="Balance" />
+          <button disabled>Approve</button>
+        </div>
+        <div class="last">
+          <h3>Deposit</h3>
+          <label for="Staked">Deposited:<span>0 TPAD</span></label>
+          <input id="Staked" />
+          <button disabled>Approve</button>
+        </div>
+        <div class="last">
+          <h3>Pending rewards</h3>
+          <label>0.00 LTY</label>
+          <button disabled>Approve</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "FarmItem",
+  data() {
+    return {
+      down: true,
+      up: false,
+    };
+  },
+  props: {
+    activeQ: String,
+    activeA: String,
+
+    image: String,
+    farmHeader: String,
+    farmSubHeader: String,
+    farmDesc: String,
+    staked: String,
+    earned: String,
+    APR: String,
+    LP: String,
+    total: String,
+  },
+  methods: {
+    toggle: function (event) {
+      event.target.classList.toggle("active");
+      event.target.nextElementSibling.classList.toggle("active");
+      this.up = !this.up;
+      this.down = !this.down;
+    },
+  },
+};
+</script>
--- src/views/FarmView.vue
+++ src/views/FarmView.vue
@@ -1,7 +1,71 @@
 <template>
-  <div class="pools">
-    <section>
-      <p class="comming">Comming Soon</p>
-    </section>
+  <div class="farm">
+    <div class="container">
+      <h2>Farm</h2>
+      <div class="flex-farm">
+        <FarmItem
+          :image="require('@/assets/images/pools/profile-img1.png')"
+          farmHeader="Ledgity LP Staking"
+          farmSubHeader="NFTPAD/NFTPAD"
+          farmDesc="Pool is deprecated, please withdraw the funds and stake in the other one."
+          staked="0 TPAD"
+          earned="0.00 TPAD"
+          APR="0"
+          LP="$0.0567"
+          total="$0"
+          activeQ="q"
+          activeA="a"
+        />
+        <FarmItem
+          :image="require('@/assets/images/pools/deadknight.1951af75.jpg')"
+          farmHeader="IDO Staking"
+          farmSubHeader="TPAD / TPAD"
+          farmDesc="Pool is deprecated, please withdraw the funds and stake in the other one."
+          staked="0 TPAD"
+          earned="0.00 TPAD"
+          APR="0"
+          LP="$0.0567"
+          total="$0"
+          activeQ="q"
+          activeA="a"
+        />
+        <FarmItem
+          :image="require('@/assets/images/pools/riox.4a340dcf.png')"
+          farmHeader="NFTPad IDO Staking"
+          farmSubHeader="NFTPAD/NFTPAD"
+          farmDesc="Pool is deprecated, please withdraw the funds and stake in the other one."
+          staked="0 TPAD"
+          earned="0.00 TPAD"
+          APR="0"
+          LP="$0.0567"
+          total="$0"
+          activeQ="q"
+          activeA="a"
+        />
+        <FarmItem
+          :image="require('@/assets/images/pools/nestarcade.533035d4.jpg')"
+          farmHeader="Ledgity LP Staking"
+          farmSubHeader="NFTPAD/NFTPAD"
+          farmDesc="Pool is deprecated, please withdraw the funds and stake in the other one."
+          staked="0 TPAD"
+          earned="0.00 TPAD"
+          APR="0"
+          LP="$0.0567"
+          total="$0"
+          activeQ="q"
+          activeA="a"
+        />
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import FarmItem from "@/components/FarmSections/FarmItem.vue";
+
+export default {
+  name: "FarmView",
+  components: {
+    FarmItem,
+  },
+};
+</script>
